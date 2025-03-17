e74a72abe6f0e7ed1c406089733dd4306c16cf79
unknown <a1793138@student.adelaide.edu.au>
2025-03-17 20:15:11 +1030

    test

---
+++ Proxy.py
@@ -0,0 +1,10 @@
+'''
+Author: yuheng li a1793138
+Date: 2025-03-17 20:14:46
+LastEditors: yuheng 
+LastEditTime: 2025-03-17 20:14:52
+FilePath: \CNA\CNAass1\Proxy.py
+Description: 
+
+Copyright (c) ${2024} by ${yuheng li}, All Rights Reserved. 
+'''
