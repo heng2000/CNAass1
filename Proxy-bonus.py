a9e164122b4a53e22f47536889cb8ade94b313b2
unknown <a1793138@student.adelaide.edu.au>
2025-03-22 21:40:13 +1030

    Proxy-bonus.py add

---
+++ Proxy-bonus.py
@@ -0,0 +1,10 @@
+'''
+Author: yuheng li a1793138
+Date: 2025-03-22 20:30:02
+LastEditors: yuheng 
+LastEditTime: 2025-03-22 20:30:08
+FilePath: \CNAass1\Proxy-bonus.py
+Description: 
+
+Copyright (c) ${2024} by ${yuheng li}, All Rights Reserved. 
+'''
