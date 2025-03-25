c2739582d9a4bcf68861d6812717ff1d13318174
unknown <a1793138@student.adelaide.edu.au>
2025-03-25 11:36:47 +1030

    ready to do bonus and list target

--- Proxy-bonus.py
+++ Proxy-bonus.py
@@ -2,9 +2,15 @@
 Author: yuheng li a1793138
 Date: 2025-03-22 20:30:02
 LastEditors: yuheng 
-LastEditTime: 2025-03-22 20:30:08
+LastEditTime: 2025-03-25 11:36:05
 FilePath: \CNAass1\Proxy-bonus.py
 Description: 
 
 Copyright (c) ${2024} by ${yuheng li}, All Rights Reserved. 
 '''
+#if exist .expires file at ht same time the expires does not overdate
+#return from cache
+#else try to re-get resource form server-update cache and .expires ifle
+#the question is how to get the timestamp of .expires
+
+#save to resource
