6090110866a82dad08871a7e0c0552ec0f4a89b8
unknown <a1793138@student.adelaide.edu.au>
2025-03-28 16:41:25 +1030

    fix for line in splitHeader type error and test successfully

--- Proxy-bonus.py
+++ Proxy-bonus.py
@@ -125,14 +125,17 @@ def getRequest(cS, url):
         saveHeader(cachePath , body,splitHeader)
         #if get html page
         #check whether ave html in respinse text
-        if b"text/html" in splitHeader:
+        #fix error splitHeader is str but i want is byte
+        #always return f
+        if any("text/html" in line.lower() for line in splitHeader):
             #string
             htmlPart =body.decode()
             linkPart =GetHrefSrc(htmlPart)
             for link in linkPart:
                 SaveHttpPage(link)
+            print("html find")
             #finish to write
-    cS.shutdown(socket.SHUT_WR)
+    #cS.shutdown(socket.SHUT_WR)
 #Look for "href=" and "src=" in the HTML. (2 marks)
 
 def GetHrefSrc(htmlContent):
@@ -198,17 +201,28 @@ if __name__ == "__main__":
             print("ero check error message", e)
         client_socket.close()
 
-        C:\Users\lenovo\Desktop\CNA\CNAass1>curl -iS "http://localhost:8080/http://httpbin.org/cache/"
-HTTP/1.1 404 NOT FOUND
-Date: Fri, 28 Mar 2025 03:43:46 GMT
-Content-Type: text/html
-Content-Length: 233
+
+C:\Users\lenovo\Desktop\CNA\CNAass1>curl -iS "http://localhost:8080/http://httpbin.org/html"
+HTTP/1.1 200 OK
+Date: Fri, 28 Mar 2025 05:20:04 GMT
+Content-Type: text/html; charset=utf-8
+Content-Length: 3741
 Connection: close
 Server: gunicorn/19.9.0
 Access-Control-Allow-Origin: *
 Access-Control-Allow-Credentials: true
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<title>404 Not Found</title>
-<h1>Not Found</h1>
-<p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+      <h1>Herman Melville - Moby-Dick</h1>
+
+      <div>
+        <p>
+          Availing himself of the mild, summer-cool weather that now reigned in these latitudes, and in preparation for the peculiarly active pursuits shortly to be anticipated, Perth, the begrimed, blistered old blacksmith, had not removed his portable forge to the hold again, after concluding his contributory work for Ahab's leg, but still retained it on deck, fast lashed to ringbolts by the foremast; being now almost incessantly invoked by the headsmen, and harpooneers, and bowsmen to do some little job for them; altering, or repairing, or new shaping their various weapons and boat furniture. Often he would be surrounded by an eager circle, all waiting to be served; holding boat-spades, pike-heads, harpoons, and lances, and jealously watching his every sooty movement, as he toiled. Nevertheless, this old man's was a patient hammer wielded by a patient arm. No murmur, no impatience, no petulance did come from him. Silent, slow, and solemn; bowing over still further his chronically broken back, he toiled away, as if toil were life itself, and the heavy beating of his hammer the heavy beating of his heart. And so it was.—Most miserable! A peculiar walk in this old man, a certain slight but painful appearing yawing in his gait, had at an early period of the voyage excited the curiosity of the mariners. And to the importunity of their persisted questionings he had finally given in; and so it came to pass that every one now knew the shameful story of his wretched fate. Belated, and not innocently, one bitter winter's midnight, on the road running between two country towns, the blacksmith half-stupidly felt the deadly numbness stealing over him, and sought refuge in a leaning, dilapidated barn. The issue was, the loss of the extremities of both feet. Out of this revelation, part by part, at last came out the four acts of the gladness, and the one long, and as yet uncatastrophied fifth act of the grief of his life's drama. He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow's technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful, daughter-like, loving wife, and three blithe, ruddy children; every Sunday went to a cheerful-looking church, planted in a grove. But one night, under cover of darkness, and further concealed in a most cunning disguisement, a desperate burglar slid into his happy home, and robbed them all of everything. And darker yet to tell, the blacksmith himself did ignorantly conduct this burglar into his family's heart. It was the Bottle Conjuror! Upon the opening of that fatal cork, forth flew the fiend, and shrivelled up his home. Now, for prudent, most wise, and economic reasons, the blacksmith's shop was in the basement of his dwelling, but with a separate entrance to it; so that always had the young and loving healthy wife listened with no unhappy nervousness, but with vigorous pleasure, to the stout ringing of her young-armed old husband's hammer; whose reverberations, muffled by passing through the floors and walls, came up to her, not unsweetly, in her nursery; and so, to stout Labor's iron lullaby, the blacksmith's infants were rocked to slumber. Oh, woe on woe! Oh, Death, why canst thou not sometimes be timely? Hadst thou taken this old blacksmith to thyself ere his full ruin came upon him, then had the young widow had a delicious grief, and her orphans a truly venerable, legendary sire to dream of in their after years; and all of them a care-killing competency.
+        </p>
+      </div>
+  </body>
+</html>
